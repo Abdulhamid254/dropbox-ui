6a9a08b7adfbd4b311be1a8730cd03e4196a4278
Abdulhamid254 <nassurabdulhamid49@gmail.com>
2023-12-02 11:14:35 -1100

    Renamemodal & Deletemodal implemented

--- components/table/Table.tsx
+++ components/table/Table.tsx
@@ -19,6 +19,8 @@ import { FileType } from "@/typings"
 import { PencilIcon, TrashIcon } from "lucide-react"
 import { Button } from "../ui/button"
 import { useAppStore } from "@/store/store"
+import { DeleteModal } from "../DeleteModal"
+import RenameModal from "../RenameModal"
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[]
@@ -77,6 +79,8 @@ export function DataTable<TData, TValue>({
                 key={row.id}
                 data-state={row.getIsSelected() && "selected"}
               >
+                <DeleteModal/>
+                <RenameModal/>
                 {row.getVisibleCells().map((cell) => (
                   <TableCell key={cell.id}>
                     {cell.column.id === "timestamp" ? (
